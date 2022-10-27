1c88c9cfc2b661161f52f0f9731f1e2d56c92e4d
student <student@lab-45-5.cs.put.poznan.pl>
2022-10-27 06:31:31 +0400

    Adde

--- 04-git/Program.java
+++ 04-git/Program.java
@@ -3,6 +3,7 @@ public class Main {
   
     public static void main(String[] args) {
       Main myObj = new Main();
+      for(int i=0;i<100;i++){
       System.out.println(myObj.x);
-    }
+    }}
   }
