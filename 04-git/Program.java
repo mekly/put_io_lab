753e90f8c913368e84a5b46173a8f1d8c92ddf49
student <student@lab-45-5.cs.put.poznan.pl>
2022-10-27 06:55:21 +0400

    modified:   Program.java
    	modified:   Program.java
    
     On branch main
    
    now

--- 04-git/Program.java
+++ 04-git/Program.java
@@ -1,9 +1,25 @@
+import java.util.ArrayList;
+import java.net.NetPermission;
 public class Main {
-    int x = 5;
-  
+  //
+    int x = 1215;
+    
+
     public static void main(String[] args) {
       Main myObj = new Main();
       for(int i=0;i<100;i++){
       System.out.println(myObj.x);
     }}
+    //Function to sum
+    public Integer Sum(ArrayList<Integer> lista ){
+        Integer result=0;
+        int siz =lista.size();
+        for(int i=0;i<size;i++){
+            result+=lista.get(i);
+
+
+        }
+        return result;
+    }
   }
+  //do something
